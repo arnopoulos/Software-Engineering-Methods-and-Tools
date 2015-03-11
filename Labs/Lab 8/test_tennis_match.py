d524a79145b8bb2ea370b044afa00d647334f970
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-11 13:16:35 -0600

    Removed redundant code

--- Labs/Lab 8/test_tennis_match.py
+++ Labs/Lab 8/test_tennis_match.py
@@ -25,107 +25,51 @@ class TestTennisGame(unittest.TestCase):
     	#1 - 3 -> 15 - 45
     	match = TennisGame()
     	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(3): match.playerTwoScored()
     	self.assertEqual("15 - 45",match.score())
 
     def test_ThreeToThree(self):
     	#3 - 3 -> deuce
     	match = TennisGame()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(3): match.playerOneScored()
+    	for i in range(3): match.playerTwoScored()
     	self.assertEqual("deuce",match.score())
 
     def test_FiveToFive(self):
     	#5 - 5 -> deuce
     	match = TennisGame()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(5): match.playerOneScored()
+    	for i in range(5): match.playerTwoScored()
     	self.assertEqual("deuce",match.score())
 
     def test_SixToFive(self):
     	#6 - 5 -> advantage player 1
     	match = TennisGame()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(6): match.playerOneScored()
+    	for i in range(5): match.playerTwoScored()
     	self.assertEqual("advantage player 1",match.score())
 
     def test_SixToSeven(self):
     	#6 - 7 -> advantage player 2
     	match = TennisGame()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(6): match.playerOneScored()
+    	for i in range(7): match.playerTwoScored()
     	self.assertEqual("advantage player 2",match.score())
 
     def test_EightToSix(self):
     	#8 - 6 -> player 1 won
     	match = TennisGame()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(8): match.playerOneScored()
+    	for i in range(6): match.playerTwoScored()
     	self.assertEqual("player 1 won",match.score())
 
     def test_ThreeToFive(self):
     	#3 - 5 -> player 2 won
     	match = TennisGame()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerOneScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
-    	match.playerTwoScored()
+    	for i in range(3): match.playerOneScored()
+    	for i in range(5): match.playerTwoScored()
     	self.assertEqual("player 2 won",match.score())
-
-
-
-
-
+    	
 if __name__ == '__main__':
     unittest.main()
 
